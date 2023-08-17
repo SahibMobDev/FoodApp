8aaa91766be09084eb330599c64ea774e68ea2f5
SahibMobDev <sahibmobdev@gmail.com>
2023-08-17 15:05:29 +0400

    Navigation Component Setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.github.sahibmobdev.foodapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.github.sahibmobdev.foodapp"
@@ -30,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 
     buildFeatures {
@@ -105,7 +105,7 @@ dependencies {
 
     // Shimmer
     implementation ("com.facebook.shimmer:shimmer:0.5.0")
-    implementation ("com.todkars:shimmer-recyclerview:0.4.1")
+    //implementation ("com.todkars:shimmer-recyclerview:0.4.1")
 
     // Jsoup
     implementation ("org.jsoup:jsoup:1.14.3")
--- app/src/main/java/com/github/sahibmobdev/foodapp/MainActivity.kt
+++ app/src/main/java/com/github/sahibmobdev/foodapp/MainActivity.kt
@@ -2,10 +2,34 @@ package com.github.sahibmobdev.foodapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.github.sahibmobdev.foodapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoriteRecipesFragment,
+                R.id.foodJokeFragment
+            ))
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
