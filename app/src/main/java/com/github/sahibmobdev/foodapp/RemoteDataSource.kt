35ae979990f31b221b6a0e9f7d1cb5724892d788
SahibMobDev <sahibmobdev@gmail.com>
2023-08-21 13:57:26 +0400

    Inject RemoteDataSource

--- app/src/main/java/com/github/sahibmobdev/foodapp/RemoteDataSource.kt
+++ app/src/main/java/com/github/sahibmobdev/foodapp/RemoteDataSource.kt
@@ -1,4 +1,14 @@
 package com.github.sahibmobdev.foodapp
 
-class RemoteDataSource() {
+import com.github.sahibmobdev.foodapp.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi: FoodRecipesApi
+) {
+
+    suspend fun getRecipes(queries: Map<String, String>) : Response<FoodRecipe> {
+        return foodRecipesApi.getRecipes(queries)
+    }
 }
