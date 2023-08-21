a5011e9002f8bb329bdd2387a1d1f5488b889b63
SahibMobDev <sahibmobdev@gmail.com>
2023-08-21 17:03:38 +0400

    Create and Inject Repository class

---
+++ app/src/main/java/com/github/sahibmobdev/foodapp/util/NetworkResult.kt
@@ -0,0 +1,12 @@
+package com.github.sahibmobdev.foodapp.util
+
+sealed class NetworkResult<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+
+    class Success<T>(data: T) : NetworkResult<T>(data)
+    class Error<T>(data: T?, message: String?) : NetworkResult<T>(data, message)
+    class Loading<T> : NetworkResult<T>()
+
+}
