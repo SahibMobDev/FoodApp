3d5fa0f20b8c96c5df677c3737e65c43749a5c64
SahibMobDev <sahibmobdev@gmail.com>
2023-08-22 14:20:44 +0400

    Create and inject MainVIewModel class.

---
+++ app/src/main/java/com/github/sahibmobdev/foodapp/MainViewModel.kt
@@ -0,0 +1,85 @@
+package com.github.sahibmobdev.foodapp
+
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.github.sahibmobdev.foodapp.data.Repository
+import com.github.sahibmobdev.foodapp.models.FoodRecipe
+import com.github.sahibmobdev.foodapp.util.NetworkResult
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    application: Application,
+    private val repository: Repository
+) : AndroidViewModel(application) {
+
+    var recipesResponse: MutableLiveData<NetworkResult<FoodRecipe>> = MutableLiveData()
+
+    fun getRecipes(queries: Map<String, String>) = viewModelScope.launch {
+        getRecipesSafeCall(queries)
+    }
+
+    private suspend fun getRecipesSafeCall(queries: Map<String, String>) {
+        if (hasInternetConnection()) {
+            try {
+                val response = repository.remote.getRecipes(queries)
+                recipesResponse.value = handleFoodRecipesResponse(response)
+            } catch (e: Exception) {
+                recipesResponse.value = NetworkResult.Error("Recipes not found.")
+            }
+
+        } else {
+            recipesResponse.value = NetworkResult.Error("No Internet Connection.")
+        }
+    }
+
+    private fun handleFoodRecipesResponse(response: Response<FoodRecipe>): NetworkResult<FoodRecipe>? {
+        recipesResponse.value = NetworkResult.Loading()
+        when {
+            response.message().toString().contains("timeout") -> {
+                return NetworkResult.Error("Timeout")
+            }
+
+            response.code() == 402 -> {
+                return NetworkResult.Error("API Key Limited.")
+            }
+
+            response.body()?.results.isNullOrEmpty() -> {
+                return NetworkResult.Error("Recipes not found.")
+            }
+
+            response.isSuccessful -> {
+                val foodRecipe = response.body()
+                return NetworkResult.Success(foodRecipe!!)
+            }
+            else -> {
+                return NetworkResult.Error(response.message())
+            }
+        }
+    }
+
+    private fun hasInternetConnection() : Boolean {
+
+        val connectivityManager = getApplication<Application>().getSystemService(
+            Context.CONNECTIVITY_SERVICE
+        ) as ConnectivityManager
+
+        val activeNetwork = connectivityManager.activeNetwork ?: return false
+        val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+
+        return when {
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> return true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> return true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> return true
+            else -> false
+        }
+    }
+}
