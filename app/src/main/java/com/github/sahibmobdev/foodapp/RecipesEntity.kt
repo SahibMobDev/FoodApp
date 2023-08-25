d769553574b2ad4f5b2bf4473447789401d63cfa
SahibMobDev <sahibmobdev@gmail.com>
2023-08-25 14:16:10 +0400

    Create Entity (RecipesEntity).

---
+++ app/src/main/java/com/github/sahibmobdev/foodapp/RecipesEntity.kt
@@ -0,0 +1,14 @@
+package com.github.sahibmobdev.foodapp
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.github.sahibmobdev.foodapp.models.FoodRecipe
+import com.github.sahibmobdev.foodapp.util.Constants.RECIPES_TABLE
+
+@Entity(tableName = RECIPES_TABLE)
+class RecipesEntity(
+    var foodRecipe: FoodRecipe
+) {
+    @PrimaryKey(autoGenerate = false)
+    val id: Int = 0
+}
---
+++ app/src/main/java/com/github/sahibmobdev/foodapp/util/Constants.kt
@@ -0,0 +1,22 @@
+package com.github.sahibmobdev.foodapp.util
+
+object Constants {
+
+    const val API_KEY = "5054361d924c4feaa0ef07c1094b1628"
+    const val BASE_URL = "https://api.spoonacular.com"
+    const val URL = "https://api.spoonacular.com/recipes/complexSearch?number=1&apiKey=5054361d924c4feaa0ef07c1094b1628&type=fingerfood&diet=vegan&addRecipeInformation=true&fillIngredients=true"
+
+
+    //API Query Keys
+    const val QUERY_NUMBER = "number"
+    const val QUERY_API_KEY = "apiKey"
+    const val QUERY_TYPE = "type"
+    const val QUERY_DIET = "diet"
+    const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
+    const val QUERY_FILL_INGREDIENTS = "fillIngredients"
+
+    //ROOM DATABASE
+    const val DATABASE_NAME = "recipes_database"
+    const val RECIPES_TABLE = "recipes_table"
+
+}
