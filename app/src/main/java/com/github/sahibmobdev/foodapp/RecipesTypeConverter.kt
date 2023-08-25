a31690e3f08ecfdd5a125cad12b0665c3c6f772f
SahibMobDev <sahibmobdev@gmail.com>
2023-08-25 14:54:04 +0400

    Create Type Converter (RecipesTypeConverter).

---
+++ app/src/main/java/com/github/sahibmobdev/foodapp/RecipesTypeConverter.kt
@@ -0,0 +1,22 @@
+package com.github.sahibmobdev.foodapp
+
+import androidx.room.TypeConverter
+import com.github.sahibmobdev.foodapp.models.FoodRecipe
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+
+class RecipesTypeConverter {
+
+    var gson = Gson()
+
+    @TypeConverter
+    fun foodRecipeToString(foodRecipe: FoodRecipe): String {
+        return gson.toJson(foodRecipe)
+    }
+
+    fun stringToFoodRecipe(data: String): FoodRecipe {
+        val listType = object : TypeToken<FoodRecipe>(){}.type
+        return gson.fromJson(data, listType)
+    }
+}
